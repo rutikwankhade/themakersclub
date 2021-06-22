d011756c41668580b5a82a78a64e56444724dca7
rutik wankhade <rutikwankhade2@gmail.com>
2021-06-22 11:13:12 +0530

    add signup ui and form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
             <h1 className=" text-gray-600 text-2xl italic font-mono font-bold "> themakersclub</h1>
             <Link to="/signup" className="ml-auto mr-2">
                
-             <button className="bg-gray-600 hover:bg-gray-700 font-semibold text-white px-6 py-2 rounded">Sign up</button>
+             <button className="  bg-gray-600 hover:bg-gray-700 font-semibold text-white px-6 py-2 rounded">Register</button>
         </Link>
         </div>
      );
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import joinImg from '../assets/images/join.png'
 const Signup = () => {
     return (
         <div>
-            signup now
+            <div className="m-20">
+                <h1 className="text-4xl font-semibold">Join the makersclub</h1>
+                <div>
+
+
+                    <div className="flex">
+                        <form className="w-1/2 py-10 px-6">
+                            <input type="text" placeholder="Name" className="m-1 bg-gray-100 p-2 w-full rounded" />
+                            <input type="email" placeholder="Email" className="m-1 bg-gray-100 p-2 w-full rounded" />
+                            <input type="password" placeholder="Password" className="m-1 bg-gray-100 p-2 w-full rounded" />
+                            <input type="password" placeholder="Confirm password" className=" m-1 bg-gray-100 p-2 w-full rounded" />
+                            <button className="m-1 mt-2 bg-indigo-400 hover:bg-indigo-500 font-semibold text-white px-6 py-2 rounded">Sign up</button>
+                            <span className="text-indigo-400 font-semibold mx-4">Already a user? Login</span>
+                        </form>
+
+                        <img src={joinImg} className="w-1/2" />
+
+                    </div>
+
+
+
+                </div>
+
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default Signup;
