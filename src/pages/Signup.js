a7ead907a904c91206a17cff6419f9eae7c54079
rutik wankhade <rutikwankhade2@gmail.com>
2021-06-22 11:39:24 +0530

    add react-form-hook to signup form

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,24 +1,70 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useForm } from "react-hook-form";
+
 import joinImg from '../assets/images/join.png'
 const Signup = () => {
+
+    const [error, setError] = useState('')
+    
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        if (data.password !== data.confirmPassword) {
+            setError('Passwords didnt match.')
+            
+        }
+        console.log(data);
+    }
+
+   
+
     return (
         <div>
             <div className="m-20">
                 <h1 className="text-4xl font-semibold">Join the makersclub</h1>
                 <div>
 
+                    <div className="flex md:flex-row flex-col">
+                        <form onSubmit={handleSubmit(onSubmit)}
+                            className="md:w-1/2 py-10 px-6">
+                        
+                            <p className="text-red-300 py-2 text-center">{error}</p>
 
-                    <div className="flex">
-                        <form className="w-1/2 py-10 px-6">
-                            <input type="text" placeholder="Name" className="m-1 bg-gray-100 p-2 w-full rounded" />
-                            <input type="email" placeholder="Email" className="m-1 bg-gray-100 p-2 w-full rounded" />
-                            <input type="password" placeholder="Password" className="m-1 bg-gray-100 p-2 w-full rounded" />
-                            <input type="password" placeholder="Confirm password" className=" m-1 bg-gray-100 p-2 w-full rounded" />
-                            <button className="m-1 mt-2 bg-indigo-400 hover:bg-indigo-500 font-semibold text-white px-6 py-2 rounded">Sign up</button>
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                className="mb-2 bg-gray-100 p-2 w-full rounded"
+                                {...register("name")}
+                            />
+
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                className="mb-2 bg-gray-100 p-2 w-full rounded"
+                                {...register("email")}
+                            />
+
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                className="mb-2 bg-gray-100 p-2 w-full rounded"
+                                {...register("password")}
+                            />
+
+                            <input
+                                type="password"
+                                placeholder="Confirm password"
+                                className=" mb-2 bg-gray-100 p-2 w-full rounded"
+                                {...register("confirmPassword")}
+
+                            />
+                            <button
+                                type="submit"
+                                className="m-1 mt-2 bg-indigo-400 hover:bg-indigo-500 font-semibold text-white px-6 py-2 rounded">Sign up</button>
                             <span className="text-indigo-400 font-semibold mx-4">Already a user? Login</span>
                         </form>
 
-                        <img src={joinImg} className="w-1/2" />
+                        <img src={joinImg} alt="join now" className="md:w-1/2" />
 
                     </div>
 
