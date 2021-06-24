c80d28f31415ff999fcb705403df28794b3402aa
rutik wankhade <rutikwankhade2@gmail.com>
2021-06-25 02:24:19 +0530

    style: add styling to active navlink in sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 import homeIcon from '../assets/icons/home.svg'
 import rocketIcon from '../assets/icons/rocket.svg'
@@ -10,21 +10,21 @@ const Sidebar = () => {
     return (
         <div className="sticky top-10 rounded-xl mx-2 w-20 flex flex-col py-10 ">
 
-            <Link to="/">
-                <img src={homeIcon} alt="Home" className="w-14 h-14 m-2 hover:bg-purple-100 p-2 rounded" />
-            </Link>
+            <NavLink to="/" className="pl-2" >
+                <img src={homeIcon} alt="Home" className="w-14 h-14 m-2  p-2 rounded" />
+            </NavLink>
 
-            <Link to="/showcase-feedback">
-                <img src={rocketIcon} alt="Home" className="w-14 h-14 m-2 hover:bg-purple-100 p-2 rounded" />
-            </Link>
+            <NavLink to="/showcase-feedback" className="pl-1" activeClassName="bg-pink-100 rounded" >
+                <img src={rocketIcon} alt="Home" className="w-14 h-14 m-2  p-2 rounded" />
+            </NavLink>
 
-            <Link to="/discussions">
-                <img src={discussIcon} alt="Home" className="w-14 h-14 m-2 hover:bg-purple-100 p-2 rounded" />
-            </Link>
+            <NavLink to="/discussions" className="pl-1" activeClassName="bg-pink-100 rounded" >
+                <img src={discussIcon} alt="Home" className="w-14 h-14 m-2  p-2 rounded" />
+            </NavLink>
 
-            <Link to="/resources">
-                <img src={resourceIcon} alt="Home" className="w-14 h-14 m-2 hover:bg-purple-100 p-2 rounded" />
-            </Link>
+            <NavLink to="/resources" className="pl-1" activeClassName="bg-pink-100 rounded" >
+                <img src={resourceIcon} alt="Home" className="w-14 h-14 m-2  p-2 rounded" />
+            </NavLink>
 
         </div>
     );
