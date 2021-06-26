93b9aa4bbb289ecf04ef92f86b412f148aa46805
rutik wankhade <rutikwankhade2@gmail.com>
2021-06-26 20:40:34 +0530

    add structure to resources page

--- src/pages/Discussions.js
+++ src/pages/Discussions.js
@@ -14,30 +14,31 @@ const Discussions = ({ getDiscussPosts, discussPosts }) => {
 
     return (
         <div className="bg-gray-50">
-             <div className="flex px-20 pt-10 bg-purple-100 my-2">
-                        <div className="w-1/2">
-                            <h1 className="text-4xl my-2 font-bold">Discuss ideas</h1>
-                            <p className="text-xl  ">Start a discuusion thread on any topic and invite people to share their thoughts and ideas.</p>
-                        <Link to="/post-discussion" className="sticky top-20">
+
+            <div className="flex px-20 pt-10 bg-purple-100 my-2">
+                <div className="w-1/2">
+                    <h1 className="text-4xl my-2 font-bold">Discuss ideas</h1>
+                    <p className="text-xl  ">Start a discuusion thread on any topic and invite people to share their thoughts and ideas.</p>
+                    <Link to="/post-discussion" className="sticky top-20">
                         <button className="px-6 my-4 cursor-pointer bg-purple-400 hover:bg-purple-500 focus:outline-none text-white rounded-full text-center p-2 text-2xl font-semibold">
                             Start a discussion
                         </button>
 
                     </Link>
-                
-                </div>
-                        <img src={discussionsImg} alt="discuss" className="ml-auto mr-2 w-1/3" />
 
-                    </div>
+                </div>
+                <img src={discussionsImg} alt="discuss" className="ml-auto mr-2 w-1/3" />
+
+            </div>
 
             <div className="bg-gray-50 p-2 rounded flex flex-row">
 
                 <div className="flex flex-col bg-gray-50 justify-center w-8/12 pb-20">
-                    
-                    
-                   
-                    
-                    
+
+
+
+
+
                     {discussPosts.data && discussPosts.data.map(post => {
                         return <Link to={`/discussions/${post.id}`}><DiscussPostcard post={post} /></Link>
 
@@ -46,7 +47,7 @@ const Discussions = ({ getDiscussPosts, discussPosts }) => {
                 </div>
 
                 <div className="w-4/12 flex flex-col items-center">
-                    
+
 
                     <div className=" sticky top-40 m-4 bg-pink-100 p-4 rounded border-t-8 border-pink-200">
 
--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -6,22 +6,54 @@ import { LinkPreview } from '@dhaiwat10/react-link-preview';
 
 const Resources = () => {
     return (
-        <div className="">
-            <div className="flex px-10 pt-10  my-2 bg-green-100">
+        <div className="bg-gray-50">
+
+
+            <div className="flex flex-row">
+
+
+                <div className="w-8/12">
+                    <div className="flex px-2 my-2 bg-purple-100">
                         <div className="m-10">
-                            <h1 className="text-4xl my-2 font-bold">Share resources</h1>
-                            <p className="text-xl ">Share helpful resources. And find them curated at one place for everyone.</p>
-                </div>
-                                        <img src={resourcesImg} alt="resources" className="w-1/3" />
+                            <h1 className="text-4xl my-2 font-bold">Resources</h1>
+                            <p className="text-xl ">A collection of resources shared by members of the makers club.</p>
+                        </div>
+                        <img src={resourcesImg} alt="resources" className="w-80 h-40 ml-auto mr02 mt-auto mb-0" />
 
                     </div>
 
-            <div>
-            share and find resources
-            <LinkPreview url='https://tabwave.vercel.app/' width="25%" className="m-2" height="250px" />
-            <LinkPreview url='https://tabwave.vercel.app/' width="25%" className="m-2" height="250px" />
 
-</div>
+
+
+                    <div className="flex flex-row flex-wrap  justify-center">
+                        <LinkPreview url='https://tabwave.vercel.app/' width="30%" descriptionLength="40" className=" m-2 text-sm" height="220px" />
+                        <LinkPreview url='https://dev.to' descriptionLength="40" width="30%" className="m-2 text-sm" height="220px" />
+                        <LinkPreview url='https://designvalley.club' descriptionLength="40" width="30%" className="m-2 text-sm" height="220px" />
+                        <LinkPreview url='https://tabwave.vercel.app/' width="30%" descriptionLength="40" className=" m-2 text-sm" height="220px" />
+                        <LinkPreview url='https://tabwave.vercel.app/' width="30%" descriptionLength="40" className=" m-2 text-sm" height="220px" />
+                        <LinkPreview url='https://tabwave.vercel.app/' width="30%" descriptionLength="40" className=" m-2 text-sm" height="220px" />
+                        <LinkPreview url='https://tabwave.vercel.app/' width="30%" descriptionLength="40" className=" m-2 text-sm" height="220px" />
+                        <LinkPreview url='https://tabwave.vercel.app/' width="30%" descriptionLength="40" className=" m-2 text-sm" height="200px" />
+                        <LinkPreview url='https://html-css-js.com/html/character-codes/' width="30%" descriptionLength="40" className=" m-2 text-sm" height="200px" />
+                        <LinkPreview url='https://tabwave.vercel.app/' width="30%" descriptionLength="40" className=" m-2 text-sm" height="200px" />
+
+                    </div>
+
+                </div>
+
+
+                <div className="sticky top-20 h-60 bg-white border-2 p-6 m-4  w-4/12">
+                    <input
+                        placeholder="Share a resource url"
+                        className="text-xl bg-gray-50 border-2 w-full p-2 rounded" />
+
+
+                   
+                    <button className=" flex mx-auto text-xl bg-gray-700 text-white p-2 px-4 my-2 rounded">Submit</button>
+                </div>
+
+
+            </div>
         </div>
     );
 }
