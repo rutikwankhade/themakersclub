0cfe83517f84e19025c9196c750d5323603a2e3a
rutik wankhade <rutikwankhade2@gmail.com>
2021-06-26 19:25:39 +0530

    style: update sidebar activaclass link

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,15 +14,15 @@ const Sidebar = () => {
                 <img src={homeIcon} alt="Home" className="w-14 h-14 m-2  p-2 rounded" />
             </NavLink>
 
-            <NavLink to="/showcase-feedback" className="pl-1" activeClassName="bg-pink-100 rounded" >
+            <NavLink to="/showcase-feedback" className="pl-1" activeClassName="bg-purple-100 rounded" >
                 <img src={rocketIcon} alt="Home" className="w-14 h-14 m-2  p-2 rounded" />
             </NavLink>
 
-            <NavLink to="/discussions" className="pl-1" activeClassName="bg-pink-100 rounded" >
+            <NavLink to="/discussions" className="pl-1" activeClassName="bg-purple-100 rounded" >
                 <img src={discussIcon} alt="Home" className="w-14 h-14 m-2  p-2 rounded" />
             </NavLink>
 
-            <NavLink to="/resources" className="pl-1" activeClassName="bg-pink-100 rounded" >
+            <NavLink to="/resources" className="pl-1" activeClassName="bg-purple-100 rounded" >
                 <img src={resourceIcon} alt="Home" className="w-14 h-14 m-2  p-2 rounded" />
             </NavLink>
 
