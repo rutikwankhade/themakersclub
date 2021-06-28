a0911a2049cb1170a284e0bf59937a6892428727
rutik wankhade <rutikwankhade2@gmail.com>
2021-06-29 00:11:44 +0530

    home page responsive and sdd maker details

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ideaImg from '../assets/images/idea.png'
 const Header = () => {
     return (
-        <div className="p-0 bg-purple-400 w-full h-80 rounded-xl text-center flex flex-row justify-center items-center">
+        <div className="p-0 pt-12 md:pt-1 bg-purple-400 w-full md:h-80 rounded-xl text-center flex md:flex-row flex-col justify-center items-center">
             <div className="text-white font-mono  ml-16 text-left">
                 <h1 className="text-3xl font-semibold">A club of Ideas, feedbacks and resources.</h1>
 
             </div>
 
-            <img src={ideaImg} alt="cover" className=" h-80 mr-2 ml-auto" />
+            <img src={ideaImg} alt="cover" className=" md:h-80 mr-2 ml-auto" />
 
         </div>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Header from '../components/Header'
 
-
-
 import feedbackImg from '../assets/images/review.png'
 import discussionsImg from '../assets/images/discussions.png'
 import resourcesImg from '../assets/images/resources.png'
+import twitterIcon from '../assets/icons/twitter.svg'
 
 
 const Home = () => {
@@ -13,50 +12,64 @@ const Home = () => {
         <div>
 
             <Header />
-            {/* <div className="bg-white border-2 border-gray-600 rounded w-10/12 p-10  mx-auto">
-                <p className="text-2xl text-center font-semibold">👨‍💻 Build + 📝 Get Feedback +  🤹 Learn =  ✨ Magic     </p>
-            </div> */}
-
-
-            {/* <div className="flex flex-row">
-            
-                <div className="  m-10 bg-green-200 rounded-full w-40 h-40 text-7xl flex justify-center items-center">👨‍💻 </div>
-                <div className=" m-10 bg-pink-200 rounded-full w-40 h-40 text-7xl flex justify-center items-center">👨‍💻 </div>
-                <div className=" m-10 bg-indigo-200 rounded-full w-40 h-40 text-7xl flex justify-center items-center">👨‍💻 </div>
-
-          
-            </div> */}
+           
 
             <div className="mt-20 ">
                 <h1 className="text-4xl text-center mb-10 font-semibold">How it works?</h1>
 
-                <div className="flex flex-col justify-center">
-                   
-                    <div className="flex px-10 pt-24  my-2">
-                        <img src={feedbackImg} alt="review" className="w-1/2" />
-                        <div className="m-12">
+                <div className="flex flex-col justify-center mb-40 md:mb-4">
+
+                    <div className="flex flex-col md:flex-row px-10 pt-24  my-2">
+                        <img src={feedbackImg} alt="review" className="md:w-1/2" />
+                        <div className="md:m-12 my-4">
                             <h1 className="text-4xl my-2 font-bold">Showcase and ask for feedback</h1>
                             <p className="text-xl ">Showcase what you built and get constructive feedback from the members of the makersclub.</p>
                         </div>
                     </div>
 
 
-                    <div className="flex px-10 pt-24  my-2">
-                        <div className="m-12">
+                    <div className="flex flex-col-reverse md:flex-row px-10 pt-24 my-2">
+                        <div className="md:m-12 my-4">
                             <h1 className="text-4xl my-2 font-bold">Discuss ideas</h1>
-                            <p className="text-xl  ">Start a discuusion thread on a topic and invite people to share their thoughts and ideas.</p>
+                            <p className="text-xl">Start a discuusion thread on a topic and invite people to share their thoughts and ideas.</p>
                         </div>
-                        <img src={discussionsImg} alt="discuss" className="w-1/2" />
+                        <img src={discussionsImg} alt="discuss" className="md:w-1/2" />
 
                     </div>
 
 
 
-                    <div className="flex px-10 pt-24  my-2">
-                        <img src={resourcesImg} alt="resources" className="w-1/2" />
-                        <div className="m-12">
+                    <div className="flex flex-col md:flex-row px-10 pt-24  my-2">
+                        <img src={resourcesImg} alt="resources" className="md:w-1/2" />
+                        <div className="md:m-12 my-4">
                             <h1 className="text-4xl my-2 font-bold">Share resources</h1>
                             <p className="text-xl ">Share helpful resources. And find them curated at one place for everyone.</p>
+                        </div>
+                    </div>
+
+
+                    <div className="border p-10 rounded md:m-10 m-4 flex">
+
+                        <div className=" md:text-left flex md:flex-row flex-col">
+                            <img src="https://rutikwankhade.dev/rutikwankhade.webp" alt="rutik wankhade"
+                                className="rounded-full h-24 w-24 m-4 opacity-80 md:ml-auto mr-2 " />
+                            
+                            <div className="m-4 md:w-2/3">
+                                <h1 className="text-2xl font-semibold">Meet the Maker</h1>
+                                <p className="text-xl py-2">👋 Hi I am Rutik. I am a Frontend Developer, an avid learner and passionate about building things with code.</p>
+
+                                <div className="flex w-32 ml-0 rounded bg-blue-400 hover:bg-blue-500 text-xl text-white px-2 py-1 mx-auto">
+                                    <img src={twitterIcon} alt="twitter" className="h-8 w-8  text-white mx-2" />
+
+                                    <a className="twitter-follow-button "
+                                        href="https://twitter.com/WankhadeRutik"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        data-size="large">
+                                        Follow </a>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 </div>
