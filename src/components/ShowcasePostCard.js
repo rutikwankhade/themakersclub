4313f55d6519f601a728b30d73b5fddb63dc9289
rutik wankhade <rutikwankhade2@gmail.com>
2021-09-19 22:26:50 +0530

    feat: update showcase posts with screenshots

--- src/components/ShowcasePostCard.js
+++ src/components/ShowcasePostCard.js
@@ -11,17 +11,24 @@ const ShowcasePostcard = ({ post: { _id, title, url, user, name, desc } }) => {
 
                 <div className="flex flex-row items-center">
 
+
+
                     <div className="md:w-10/12">
-                        <h1 className=" capitalize text-xl px-4 font-semibold">{title && title}</h1>
+                        <h1 className=" capitalize text-2xl px-4 font-semibold">{title && title}</h1>
                         <p className="text-lg p-4 mb-2 ">{desc}</p>
                     </div>
 
-                    <div className="w-20 h-20 ml-auto mr-2 text-2xl border flex flex-col justify-center items-center">
+                    {/* <div className="w-20 h-20 ml-auto mr-2 text-2xl border flex flex-col justify-center items-center">
                         <span className="text-xl font-bold">12</span>
                         <span className="opacity-70">
                             ❤
                         </span>
+                    </div> */}
+
+                    <div className="p-2 w-1/3 my-2 border rounded shadow-sm">
+                        <img src={`https://capture-website-api.herokuapp.com/capture?url=${url}`} alt="screenshot" className=" my-2" />
                     </div>
+
 
                 </div>
             </Link>
