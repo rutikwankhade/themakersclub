369252bef51fc10f84b0f2213d4d81559027d2f1
rutik wankhade <rutikwankhade2@gmail.com>
2021-09-19 22:49:44 +0530

    feat: update header of the leaderboard

--- src/pages/LeaderBoard.js
+++ src/pages/LeaderBoard.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import MakerCard from '../components/MakerCard'
+import leaderboardImg from '../assets/images/leaderboard.png'
 const LeaderBoard = () => {
     return (
         <div>
             <div>
-                <h1 className="text-2xl font-semibold bg-purple-400 text-white p-6 rounded">Top makers in the club</h1>
+                 <div className="flex md:flex-row flex-col bg-purple-100 my-2 ">
+                        <div className=" m-10 md:w-1/2">
+                            <h1 className="text-4xl my-2 font-bold">Leaderboard</h1>
+                            <p className="text-xl ">Top makers from the makersclub who are building cool stuff and helping others in the community.</p>
+                        </div>
+                        <img src={leaderboardImg} alt="leaderboard" className=" md:w-80  mt-auto mb-0 ml-auto mr-2 " />
+                    </div>
             </div>
 
-            <div className="p-10">
+            <div className="">
                 <MakerCard />
                 <MakerCard />
                 <MakerCard />
