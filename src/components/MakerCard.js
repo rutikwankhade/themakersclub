7a6e790933b0738b2d5ff14b2d948b97e603230c
rutik wankhade <rutikwankhade2@gmail.com>
2021-09-21 21:25:44 +0530

    feat: add guidellines about maker points system

--- src/components/MakerCard.js
+++ src/components/MakerCard.js
@@ -2,16 +2,16 @@ import React from 'react';
 const MakerCard = ({maker, rank}) => {
     return (
         <div>
-            <div className="w-8/12 cursor-pointer hover:bg-purple-50 border p-6 rounded flex flex-row items-center">
-                <span className="text-3xl  font-bold "> { rank}</span>
+            <div className="cursor-pointer hover:bg-purple-50 border p-6 rounded flex flex-row items-center">
+                <span className="text-3xl  font-bold "> #{ rank}</span>
 
                 <div className="flex items-center ml-6 mr-auto">
                     <img src={maker.user.avatar} alt="profile" className="rounded-full w-10 h-10 mx-2" />
                     <h2 className="text-xl mx-2 capitalize">{maker.user.name}</h2>
                 </div>
 
-                <span className="text-2xl px-2 text-green-400 font-semibold">{maker.points}</span>
-                <span className="text-xl">🚀 </span>
+                <span className="text-2xl px-2 text-purple-400 font-semibold">{maker.points}</span>
+                <span className="text-xl">🚀</span>
             </div>
         </div>
     );
--- src/pages/LeaderBoard.js
+++ src/pages/LeaderBoard.js
@@ -4,7 +4,7 @@ import leaderboardImg from '../assets/images/leaderboard.png'
 
 import { connect } from 'react-redux';
 import { getTopMakers } from '../actions/profile'
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Skeleton from 'react-loading-skeleton';
 
 
@@ -28,16 +28,52 @@ const LeaderBoard = ({ loading, getTopMakers, topMakers }) => {
                 </div>
             </div>
 
-            <div className="">
-                {
-                    topMakers.map((maker, index) => {
-                        
-                        return <Link to={`/makers/${maker._id}`}>
-                            {loading ? <Skeleton height={200} /> : <MakerCard maker={maker} rank={ index+1}/>}
+
+            <div className="flex md:flex-row flex-col">
+
+
+                <div className="md:w-8/12 md:pb-20">
+                    {
+                        topMakers.map((maker, index) => {
+
+                            return <Link to={`/makers/${maker._id}`}>
+                                {loading ? <Skeleton height={200} /> : <MakerCard maker={maker} rank={index + 1} />}
                             </Link>
-                    })
-                }
-               
+                        })
+                    }
+
+                </div>
+
+                <div className="md:w-1/3 mx-4 border bg-green-50 p-4">
+                    <h1 className="text-2xl font-bold">How this works?</h1>
+                    <p className="text-md py-2">The leaderboard is based on the maker points system. For every activity, a maker is rewarded with following points.</p>
+
+
+                    <div className="transition duration-300 transform hover:scale-105 flex w-full bg-white p-2 text-md font-semibold border ">
+                        <span >Showcase your project</span>
+                        <span className="ml-auto mr-2">10 🚀</span>
+                    </div>
+
+                    <div className="transition duration-300 transform hover:scale-105 flex w-full bg-white p-2 text-md font-semibold border ">
+                        <span >Give feedback</span>
+                        <span className="ml-auto mr-2">7 🚀</span>
+                    </div>
+
+                    <div className="transition duration-300 transform hover:scale-105 flex w-full bg-white p-2 text-md font-semibold border ">
+                        <span >Start a discussion</span>
+                        <span className="ml-auto mr-2">5 🚀</span>
+                    </div>
+
+                    <div className="transition duration-300 transform hover:scale-105 flex w-full bg-white p-2 text-md font-semibold border ">
+                        <span >Engage in a discussion</span>
+                        <span className="ml-auto mr-2">3 🚀</span>
+                    </div>
+
+
+                </div>
+
+
+
             </div>
 
         </div>
